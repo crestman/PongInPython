cce4f675b7e9ac42fc1a6b6d67bc493f9d8a6e88
crestman <48895051+crestman@users.noreply.github.com>
2020-07-18 22:56:42 -0400

    Create pong.py

---
+++ pong.py
@@ -0,0 +1,58 @@
+from pygame import *
+SCREENWIDTH = 900
+SCREENHEIGHT = 600
+screen = display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+
+WHITE = (255,255,255)
+BGCOLOR = (0,220,160)
+BLUE = (50,100,230)
+RED = (230, 50, 100)
+
+p1Y = 250
+p2Y = 250
+paddleWidth = 30
+paddleHeight = 100
+
+ballX = 450
+ballY = 300
+ballDx = 4
+ballDy = 4
+
+running = True
+myClock = time.Clock()
+
+while running:
+    for evt in event.get():
+        if evt.type == QUIT:
+            running = False
+
+    p1Paddle = Rect(50, p1Y, paddleWidth, paddleHeight)
+    p2Paddle = Rect(850, p2Y, paddleWidth, paddleHeight)
+    ball = rect(ballX, ballY, 10 ,10 )
+
+    keys = key.get_pressed()
+    if (keys [K_w] and p1Y > 0):
+            p1Y -= 5
+    elif (keys[K_s] and p1Y+paddleHeight < SCREENHEIGHT):
+            p1Y += 5
+
+
+    if (keys [K_UP] and p2Y > 0):
+            p2Y -= 5
+    elif (keys[K_DOWN] and p2Y+paddleHeight < SCREENHEIGHT):
+            p2Y += 5
+
+
+    ballX += ballDx
+    ballY += ballDy
+    if (ball.col)
+
+    screen.fill(BGCOLOR)
+    draw.rect(screen, BLUE, p1Paddle)
+    draw.rect(screen, RED, p2Paddle)
+
+    display.flip()
+    myClock.tick(60)
+
+quit()
+
